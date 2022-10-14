c39c462e2f2c9ef9e2ee708604f57ddc73e75d2b
Guilherme Gomes da Silva <87196458+guilherme2255@users.noreply.github.com>
2022-10-14 15:39:57 -0300

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
+
 streamlit.title("My ass new")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & ROcket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
  
